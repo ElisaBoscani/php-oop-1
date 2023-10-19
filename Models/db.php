5a992351fb6313546c7aaa94ac0149ba2f8ae56d
Elisa Boscani <elisabosc90@gmail.com>
2023-10-19 17:08:19 +0200

    diviso in pagine

--- Models/db.php
+++ Models/db.php
@@ -1,1 +1,3 @@
-
+<?php
+$film_uno = new Movie(4, ["fantasy"]);
+$film_uno->name = "harry potter";
---
+++ Models/db_due.php
@@ -0,0 +1,4 @@
+<?php
+
+$film_due = new Movie(3, ["azione", "fantasy"]);
+$film_due->name = "indiana jones 5";
